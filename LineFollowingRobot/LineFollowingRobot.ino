0341ba4867e7d27eab16fdc7dbfa49f8e9af072a
Feilan Jiang <f-jiang@github.com>
2016-10-30 11:04:00 -0400

    Add hardware-based speed selection

--- LineFollowingRobot/LineFollowingRobot.ino
+++ LineFollowingRobot/LineFollowingRobot.ino
@@ -8,15 +8,20 @@
  */
 #include "encoder.h"
 
-#define ENC_A       A4
-#define ENC_B       A5
-#define RIGHT       0
-#define LEFT        1
-#define epsilon     50
+#define ENC_A         A4
+#define ENC_B         A5
+#define RIGHT         0
+#define LEFT          1
+#define epsilon       50
+#define N_SP_PRSETS   3
 
 int PR[] = {A0, A1};
 int motor[] = { 10, 9 };
 int lastShade[] = {0, 0};
+
+const int speedPresets[N_SP_PRSETS] = { 50, 150, 250 }; // arbitrary values for now
+const int speedCtrlPins[N_SP_PRSETS] = { 2, 4, 7 };
+int speed = 0;  // robot will not move if no pins are set
 
 bool correcting = false;
 
@@ -24,7 +29,7 @@ Encoder enc(ENC_A, ENC_B);
 
 void drive (int left, int right)  {
   analogWrite(motor[LEFT], left);
-  analogWrite(motor[RIGHT], right);   
+  analogWrite(motor[RIGHT], right);
 }
 
 void followLine() {
@@ -32,21 +37,21 @@ void followLine() {
   int lShade = analogRead(PR[LEFT]);
 
   if (abs(lShade - rShade) < epsilon) {
-    drive(255, 255);
+    drive(speed, speed);
     if (correcting) {
       correcting = false;
       Serial.write("back to equal speed");
     }
   }
   else if(lShade - rShade > epsilon) {
-    drive(0, 255);
+    drive(0, speed);
     if (!correcting) {
       Serial.write("R Full Speed");
       correcting = true;
     }
   }
   else if(rShade - lShade > epsilon) {
-    drive(255, 0);
+    drive(speed, 0);
     if (!correcting) {
       Serial.write("L Full Speed");
       correcting = true;
@@ -60,22 +65,31 @@ void followLine() {
 }
 
 void setup() {
+  int i;
+  for (i = 0; i < N_SP_PRSETS; i++) {
+    pinMode(speedCtrlPins[i], INPUT);
+  }  
+  for (i = 0; i < N_SP_PRSETS; i++) {
+    if (digitalRead(speedCtrlPins[i]) == HIGH) {
+      speed = speedPresets[i];
+      break;
+    }
+  }
+  
   enc.write(0);
   enc.start();
 
-  pinMode(8, OUTPUT);
-  digitalWrite(8, HIGH);
-  
   Serial.begin(9600); // set up communication
   pinMode(motor[RIGHT], OUTPUT);
   pinMode(motor[LEFT], OUTPUT);  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   followLine();
 
   Serial.write("\tEncoder: ");
-  Serial.println(enc.read());
+  Serial.print(enc.read());
+  Serial.write("\tSpeed: ");
+  Serial.println(speed);
 }
 
